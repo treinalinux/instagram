49764fd36f11c226aea8f199c33df91d6724e6e5
Alan da Silva Alves <linuxfama@gmail.com>
2021-07-19 18:00:45 -0300

    feat: action cable ande file adjustments

--- config/application.rb
+++ config/application.rb
@@ -32,8 +32,8 @@ module Instagram
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
-    config.time_zone = 'Brasilia'
     config.i18n.default_locale = 'pt-BR'
+    config.time_zone = 'Brasilia'
 
     # Don't generate system test files.
     config.generators.system_tests = nil
