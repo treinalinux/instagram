c97433fa1fe1482ef9afca2abb0ef2b8531491f5
Alan da Silva Alves <linuxfama@gmail.com>
2021-07-19 18:43:23 -0300

    feat: action cable and file adjustments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,19 @@
+# frozen_string_literal: true
+
 class CommentsController < ApplicationController
   def create
-    @comment = current_user.comments.create(comment_params)
+    @comment = current_user.comments.new(comment_params)
 
     if @comment.save
+      CommentChannel.broadcast_to('comment_channel',
+                                  post_id: @comment.post_id,
+                                  comment_created: render_to_string(partial: @comment))
+
       redirect_to @comment.post, notice: 'Comentário enviado com sucesso.'
     else
+      @post = @comment.post
       flash.now[:alert] = @comment.errors.full_messages.to_sentence
-      render '/posts/show'
+      render 'posts/show'
     end
   end
 
