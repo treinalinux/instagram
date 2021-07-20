63f8f99ff2108de2815ae2927baaf99c904558ca
Alan da Silva Alves <linuxfama@gmail.com>
2021-07-20 07:52:54 -0300

    BUG: bug fix on helper use combine_options rails 6.0 for 6.1

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,7 +7,9 @@ module ApplicationHelper
 
   def user_avatar(user)
     avatar = if user.avatar.attached?
-               user.avatar.variant(combine_options: { resize: '100x100^', extent: '100x100' })
+               user.avatar.variant(resize_to_fill: [100, 100])
+               # BUG FIX - Rails 6.0 for 6.1
+               # user.avatar.variant(combine_options: { resize: '100x100^', extent: '100x100' })
              else
                'default-avatar.jpg'
              end
